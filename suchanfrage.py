27d8cd037f16af9597d34b414f03388e1457203c
kastein <36165516+kastein@users.noreply.github.com>
2018-11-29 14:43:16 +0100

    Beispielsuchanfrage für Quelltext

---
+++ suchanfrage.py
@@ -0,0 +1,20 @@
+import urllib2
+
+def suchanfrage(frage):
+    l = frage.split(' ')
+    s = "https://www.chefkoch.de/rs/s0/"
+    i = 0
+    while i < len(l):
+        s = s + l[i]
+        i = i + 1
+    s = s + "/Rezepte.html"
+    quell = urllib2.urlopen(s)
+    html = quell.read()
+    with open("testquelle.txt", "w") as f:
+        f.write(html)
+    return None 
+
+
+
+
+
