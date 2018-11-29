aaa9cbff7a791cc89d8c45e7a2f14845000c8429
kastein <36165516+kastein@users.noreply.github.com>
2018-11-29 17:15:18 +0100

    Update suchanfrage.py

--- suchanfrage.py
+++ suchanfrage.py
@@ -3,11 +3,12 @@ import urllib2
 def suchanfrage(frage):
     l = frage.split(' ')
     s = "https://www.chefkoch.de/rs/s0/"
-    i = 0
+    s = s + l[0]
+    i = 1
     while i < len(l):
         s = s + l[i]
         i = i + 1
-    s = s + "/Rezepte.html"
+    s = s + "+" + "/Rezepte.html"
     quell = urllib2.urlopen(s)
     html = quell.read()
     with open("testquelle.txt", "w") as f:
