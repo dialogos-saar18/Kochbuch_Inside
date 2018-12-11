1253ae921f86a60606d3c4715b497cb2920b42a3
kastein <36165516+kastein@users.noreply.github.com>
2018-12-11 13:34:46 +0100

    Add files via upload

---
+++ rezept_class.py
@@ -0,0 +1,151 @@
+from bs4 import BeautifulSoup as bs
+
+class Recipe:
+    
+    def __init__(self, _quellcode):
+        with open(_quellcode) as f:
+            soup = bs(f,features="html.parser")
+        self.titel = self.init_titel(soup)
+        self.anleitung = self.init_anleitung(soup)
+        self.schritt = 0
+        self.zutaten = self.init_zutaten(soup)
+        self.portionen = self.init_portionen(soup)
+        #eigenschaften: dict; available keys:
+        #zubereitungszeit ebenfalls evtl. in Größe und Einheit unterteilen
+            #-> an den jeweiligen Nutzer anpassen
+        self.eigenschaften = self.init_properties(soup)
+
+    def get_schritt(self, argument): #returns string
+        #if argument == "all":
+        if argument == "next":
+            self.schritt += 1
+        elif argument == "repeat":
+            pass
+        else:
+            print ("Unerwartetes Argument "+argument+" in get_schritt")
+            ###raise Error
+
+
+    def get_zutat(self,bezeichnung): #returns string
+        #if bezeichnung == "all"
+        return str(self.zutaten[bezeichnung]['menge']) + self.zutaten[bezeichnung]['einheit']
+
+    def get_property(self,key): #returns string
+        return self.eigenschaften[key]
+
+    def get_factor():
+        pass
+
+    def umrechnen(self): # argument um faktor auszurechnen
+        factor = get_factor()
+        for d in self.zutaten:
+            d[u'menge'] = d[u'menge'] * factor
+
+    def contains(self, zutat):
+        if zutat in self.zutaten:
+            return "Ja"
+        else:
+            return "Nein"
+
+    def init_titel(self, beautifuls):
+        t = beautifuls.find("title")
+        t = t.contents[0]
+        return t[:-14]
+
+    # returns liste: [u'Die Zwiebeln fein...', u'Das Tomatenmark...',...]
+    def init_anleitung(self, beautifuls):
+        schritte = []
+        zubereitung = beautifuls.find(id="rezept-zubereitung")
+        for string in zubereitung.stripped_strings:
+            schritte.append(string)
+        return schritte
+
+    # return dict: {u'Zwiebel(n)': {u'menge': 1, u'einheit': u'm.-gro\xdfe'}, u'Zucker':...}
+    def init_zutaten(self, beautifuls):
+        zutaten = {}
+        incr = beautifuls.find("table", {"class": "incredients"})
+        stripped2 = incr.stripped_strings
+        strings = incr.strings
+        l = []
+        angaben_m = []
+        angaben_z = []
+        for i in incr.contents:
+            if str(type(i)) == "<class 'bs4.element.NavigableString'>":
+                pass
+            else:
+                li = i.findAll("td")
+                menge = li[0].contents
+                mengen_angabe= menge[0]
+
+                mengen_angabe = mengen_angabe.strip()
+                angaben_m.append(mengen_angabe)
+                    
+                zutat = li[1].contents
+                if len(zutat) == 1:
+                    zutat_angabe = zutat[0]
+                else:
+                    zutat_angabe = zutat[1].contents[0]
+                angaben_z.append(zutat_angabe.strip())
+        i = 0
+        while i < len(angaben_m):
+            m = angaben_m[i]
+            z = angaben_z[i]
+            d = {}
+            if m == u'':
+                d[u'menge'] = 0
+                d[u'einheit'] = u''
+            else:
+                m = m.replace(u'\xa0', u' ')
+                spl = m.split()
+                zahl = spl[0]
+                if len(spl) == 2:
+                    einheit = m.split()[1]
+                else:
+                    einheit = u''
+                try:
+                    d[u'menge'] = int(zahl)
+                    d[u'einheit'] = einheit
+                except:
+                    d[u'menge'] = 0
+                    d[u'einheit'] = zahl
+            zutaten[z] = d
+            i = i+1
+        return zutaten
+
+    # returns int
+    def init_portionen(self, beautifuls):
+        p = beautifuls.find("input", {"name": "portionen"})
+        p = p[u'value']
+        return p
+
+    #returns dict: {u'schwierigkeitsgrad': u'simpel', ...}
+    def init_properties(self, beautifuls):
+        preparation = {}
+        prep = beautifuls.find(id="preparation-info")
+        l = []
+        stripped = prep.stripped_strings
+        i = 0
+        for s in stripped:
+            if i % 2 == 0:
+                key = s
+            else:
+                val = u''
+                for n in s:
+                    if n == u'/':
+                        break
+                    else:
+                        n = n.strip()
+                        val = val + n
+                        
+                preparation[key] = val
+            i = i + 1
+        #preparation[u'Gesamtzeit'] = 
+        return preparation
+                                             
+
+'''
+class Nutzer:
+    def __init__(self, name):
+        load_user(name)
+        self.handicap #wie viel länger oder kürzer braucht der nutzer (in Prozent)
+'''
